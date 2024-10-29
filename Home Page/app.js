8a2fbd9f4a34bee04d19be9635e8edfa29d11dfd
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-10-29 20:38:08 +0100

    Add files via upload

--- Home Page/app.js
+++ Home Page/app.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 
 	 $(window).scroll(function(){
 		if ($(this).scrollTop() > 10) {
-			$('header').css("background-color","rgba(34,123,165,.8)");
+			//$('header').css("background-color","rgba(240, 126, 19,.8)");
 		} else {
 			$('header').css("background-color","rgba(255,255,255,0)");
 		}
@@ -124,3 +124,59 @@ function init() {
 
 }
 window.setInterval(init, 2000);
+
+particlesJS("particles-js", {
+	particles: {
+	  number: {
+		value: 180,
+		density: {
+		  enable: true,
+		  value_area: 800,
+		},
+	  },
+	  color: {
+		value: "#fff",
+	  },
+	  shape: {
+		type: "circle",
+	  },
+	  opacity: {
+		value: 0.3,
+		random: false,
+		anim: {
+		  enable: false,
+		  speed: 4,
+		  opacity_min: 0.1,
+		  sync: false,
+		},
+	  },
+	  size: {
+		value: 4,
+		random: true,
+		anim: {
+		  enable: true,
+		  speed: 2,
+		  size_min: 0.1,
+		  sync: false,
+		},
+	  },
+	  line_linked: {
+		enable: false,
+	  },
+	  move: {
+		enable: true,
+		speed: 0.4,
+		direction: "right",
+		random: true,
+		straight: false,
+		out_mode: "none",
+		bounce: false,
+		attract: {
+		  enable: false,
+		  rotateX: 600,
+		  rotateY: 1200,
+		},
+	  },
+	},
+	retina_detect: true,
+  });
