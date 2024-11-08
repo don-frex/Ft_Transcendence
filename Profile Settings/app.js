6beab6f1fb5942f6607fe32f4cd069cf43539430
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-11-08 12:45:38 +0100

    Add files via upload

--- Profile Settings/app.js
+++ Profile Settings/app.js
@@ -1,3 +1,10 @@
+let profile_pic = document.getElementById("profile-pic");
+let input_file = document.getElementById("input-file");
+
+input_file.onchange = function(){
+	profile_pic.src = URL.createObjectURL(input_file.files[0]);
+}
+
 particlesJS("particles-js", {
 	particles: {
 	  number: {
