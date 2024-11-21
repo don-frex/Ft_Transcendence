7387028157a63e6109846dfe9f89b8cab6011440
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-11-21 22:21:37 +0100

    Add files via upload

--- Components/nav-bar.js
+++ Components/nav-bar.js
@@ -1,33 +1,47 @@
 class NavBar extends HTMLElement {
 	constructor() {
-	  super();
-	  const shadow = this.attachShadow({ mode: 'open' });
-	  shadow.innerHTML = `
-		<style>
-		  nav {
-			display: flex;
-			justify-content: space-between;
-			padding: 10px;
-			background-color: #333;
-			color: white;
-		  }
-		  .logo {
-			height: 50px;
-		  }
-		  .user-pic {
-			border-radius: 50%;
-			cursor: pointer;
-		  }
-		</style>
+		super();
+		// const LinkEelement = document.createElement("link");
+		// LinkEelement.setAttribute("rel", "stylesheet");
+		// LinkEelement.setAttribute("href", "../Home Page/style.css");
+		this.shadow = this.attachShadow({ mode: 'open' });
+		// this.shadow.appendChild(LinkEelement);
+		this.shadow.innerHTML = `
+		<link rel="stylesheet" href="../Home Page/style.css">
+		<div class="body-form">
 		<nav>
-		  <a href="Home Page.html">
-			<img class="logo" src="../images/TR-logo.png" alt="Logo">
-		  </a>
-		  <img src="https://cdn.intra.42.fr/users/6346b9909265439cf973f7d7ecb87989/asaber.jpg" 
-			   class="user-pic" onclick="toggleMenu()" alt="User Picture">
+			<a href="Home Page.html">
+				<img class="logo" src="../images/TR-logo.png">
+			</a>
+				<img src="https://cdn.intra.42.fr/users/6346b9909265439cf973f7d7ecb87989/asaber.jpg" class="user-pic" onclick="toggleMenu()">
+			<div class="sub-menu-wrap" id="subMenu">
+					<div class="sub-menu">
+						<div class="user-info">
+							<img src="https://cdn.intra.42.fr/users/6346b9909265439cf973f7d7ecb87989/asaber.jpg">
+							<h2>Adnane Saber</h2>
+						</div>
+						<hr>
+						<a href="Profile Page.html" class="sub-menu-link">
+							<img src="../images/user-icon.png">
+							<p> Profile</p>
+							<span>></span>
+						</a>
+						<a href="Profile Settings.html" class="sub-menu-link">
+							<img src="../images/setting-icon.png">
+							<p> Settings</p>
+							<span>></span>
+						</a>
+						<a href="../index.html" class="sub-menu-link">
+							<img src="../images/og-out-icon.png">
+							<p> Log out</p>
+							<span>></span>
+						</a>
+					</div>
+				</div>
 		</nav>
+		</div>
 	  `;
 	}
-  }
-  
-  customElements.define('nav-bar', NavBar);
+}
+
+customElements.define('nav-bar', NavBar);
