297ee7b612856bc472c20585b97437cff63430d9
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-11-27 23:59:28 +0100

    Delete Profile Settings directory

--- Profile Settings/app.js
+++
@@ -1,6 +0,0 @@
-let profile_pic = document.getElementById("profile-pic");
-let input_file = document.getElementById("input-file");
-
-input_file.onchange = function(){
-	profile_pic.src = URL.createObjectURL(input_file.files[0]);
-}
